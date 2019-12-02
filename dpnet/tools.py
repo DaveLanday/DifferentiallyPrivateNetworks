e5a39a68a2e9ef980560c74ba7a9d2c1f826fc3b
DaveLanday <14958778+DaveLanday@users.noreply.github.com>
2019-12-02 11:32:30 -0500

    added clipGraph() function to tools.py. Used clipGraph() to prove why global sensitivity is not good for releasing edge count queries

---
+++ dpnet/tools.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*-coding: utf-8 -*-
+ 
+#tools.py
+#Dave Landay
+#LAST UPDATED: 12-02-2019
+
+import numpy as np
+from copy import deepcopy
+from . import tools
+
+def clip(arr, l=None, u=None):
+    return np.clip(arr, l, u)
+
+def clipGraph(g, b):
+    """
+        Clips the edges that have a degree which exceeds an upper bound
+
+        param: g (Graph): a networkX Graph() object
+        param: b (upper): clipping parameter, upper bound
+
+        returns: g' (Graph): a clipped network
+    """
+    g_ = deepcopy(g)
+    n2check = [(k, abs(v-b)) for k,v in g.degree() if v > b]
+    for node in n2check:
+        e = list(g_.edges(node[0]))
+        edges = np.random.choice(list(range(len(e))), size=node[1], replace=False)
+        e2remove = [e[i] for i in edges]
+        g_.remove_edges_from(e2remove)
+    return g_
